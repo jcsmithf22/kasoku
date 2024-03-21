bb57f373f634b172476c2ce56421004c9bfdf0ae
Josiah Smith <jcsmithf22@gmail.com>
2024-03-21 13:43:20 -0500

    Update masked image

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_15_150148) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_20_053155) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "remembers", force: :cascade do |t|
+    t.string "password"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.bigint "user_id", null: false
